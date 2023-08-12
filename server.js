a73a7f23c9b7d6f81323d87fe186f30225fa40f9
TanishqSingla <tanishqsingla00@gmail.com>
2023-08-12 21:20:33 +0530

    feat: Add boiler plate

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/", (_, res) => {
+  res.send("Hello world");
+})
+
+app.listen(3000, () => console.log("server is running on http://127.0.0.1:3000"))
+
