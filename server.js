efcfa2361068f15e6a2135ef03cb792713e5f91c
TanishqSingla <tanishqsingla00@gmail.com>
2023-08-13 00:46:00 +0530

    fix: lint

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
 app.get("/", (_, res) => {
-  res.send("Hello world");
-})
+	res.send("Hello world");
+});
 
-app.listen(3000, () => console.log("server is running on http://127.0.0.1:3000"))
-
+app.listen(3000, () =>
+	console.log("server is running on http://127.0.0.1:3000"),
+);
